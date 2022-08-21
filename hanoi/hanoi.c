2d3c7a76925dfb1354d838767e3fca0cc2d2adef
Arin Dhimar <101038471+arindhimar@users.noreply.github.com>
2022-08-21 10:57:23 +0530

    Add files via upload

---
+++ hanoi/hanoi.c
@@ -0,0 +1,28 @@
+/*
+this code is written by arin_dhimar
+date:21/08/2022
+*/
+
+#include<stdio.h>
+
+void hanoi(int n,char from,char to,char temp)
+{
+    if(n==1)
+    {
+        printf("\nMOVE %d disc from %c to %c",n,from,to);
+        return;
+    }
+    hanoi(n-1,from,temp,to);//CALLED FOR A SINGLE TOWER
+    printf("\nMOVE %d disc from %c to %c",n,from,to);
+    hanoi(n-1,temp,to,from);//CALLED FOR A SINGLE TOWER
+    //HENCE NEED TO BE CALLED TWICE
+}
+
+int main()
+{
+    int no;
+    printf("\nENTER NUMBER OF DISCS         ");
+    scanf("%d",&no);
+    hanoi(no,'A','C','B');
+    return 0;
+}
